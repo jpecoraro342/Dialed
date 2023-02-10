d4724380ce3eaa5715040960f5dd74164ae14f38
Joseph Pecoraro <jpecoraro342@yahoo.com>
2023-02-10 11:54:48 -0500

    Initial Commit

---
+++ Dialed/DialedApp.swift
@@ -0,0 +1,17 @@
+//
+//  DialedApp.swift
+//  Dialed
+//
+//  Created by Joseph Pecoraro on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DialedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
